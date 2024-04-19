74c9f85d2fe8c23fa9329f71c34c435872a2d83c
William Park <72711764+wcpark@users.noreply.github.com>
2024-04-18 21:07:20 -0400

    Change merge sort to take in list of tuples, where first value is playlist position and second is match percent; also, add 100,000 value test

--- src/mergesort.py
+++ src/mergesort.py
@@ -1,75 +1,79 @@
 #!/usr/bin/python3
 
+import random
 import time
 
 
 # Slides from class used as reference
-# Merges two subarray of array
-def merge(originalArray, left, mid, right):
-    # Create leftSubarray ← originalArray[left..mid] & rightSubarray ← originalArray[mid+1..right]
+# Merges two sublist of list, takes in list of tuples, where the first value is the playlist position and the second is the match percent
+def merge(originalList, left, mid, right):
+    # Create leftSublist ← originalList[left..mid] & rightSublist ← originalList[mid+1..right]
     n1 = mid - left + 1
     n2 = right - mid
 
-    # Slicing to extract subarrays from originalArray
-    leftSubarray = originalArray[left : left + n1]
-    rightSubarray = originalArray[mid + 1 : mid + 1 + n2]
+    # Slicing to extract sublists from originalList
+    leftSublist = originalList[left : left + n1]
+    rightSublist = originalList[mid + 1 : mid + 1 + n2]
 
-    # Merge the arrays leftSubarray and rightSubarray into originalArray
+    # Merge the lists leftSublist and rightSublist into originalList
     i = 0
     j = 0
     k = left
     while i < n1 and j < n2:
-        if leftSubarray[i] <= rightSubarray[j]:
-            originalArray[k] = leftSubarray[i]
+        # Compare the second values in the tuple, the match percent
+        if leftSublist[i][1] <= rightSublist[j][1]:
+            originalList[k] = leftSublist[i]
             i += 1
         else:
-            originalArray[k] = rightSubarray[j]
+            originalList[k] = rightSublist[j]
             j += 1
         k += 1
 
-    # Append the remaining elements of leftSubarray and rightSubarray
+    # Append the remaining elements of leftSublist and rightSublist
     while i < n1:
-        originalArray[k] = leftSubarray[i]
+        originalList[k] = leftSublist[i]
         i += 1
         k += 1
 
     while j < n2:
-        originalArray[k] = rightSubarray[j]
+        originalList[k] = rightSublist[j]
         j += 1
         k += 1
 
 
-def mergeSort(originalArray, left, right):
+def mergeSort(originalList, left, right):
     if left < right:
-        # m is the point where the array is divided into two subarrays
+        # m is the point where the list is divided into two sublists
         mid = (left + right) // 2
 
-        mergeSort(originalArray, left, mid)
-        mergeSort(originalArray, mid + 1, right)
+        mergeSort(originalList, left, mid)
+        mergeSort(originalList, mid + 1, right)
 
-        # Merge the sorted subarrays
-        merge(originalArray, left, mid, right)
+        # Merge the sorted sublists
+        merge(originalList, left, mid, right)
 
 
-def printArray(array):
-    for i in array:
+def printList(list):
+    for i in list:
         print(i, end=" ")
     print()
 
 
 def main():
     # Example use
-    originalArray = [8.6, 23.2, 6.73, 5.33, 0.72, 0.99]
+
+    # Generate 100,000 tuples list for testing using list comprehension
+    originalList = [(i, random.uniform(0, 1)) for i in range(1, 100001)]
 
     start_time = time.time()
-    mergeSort(originalArray, 0, len(originalArray) - 1)
+    mergeSort(originalList, 0, len(originalList) - 1)
     end_time = time.time()
 
     sort_time = end_time - start_time
 
+    print("Sorted list:")
+    printList(originalList)
     print("Time taken for merge sort:", sort_time, "seconds")
-    print("Sorted array:")
-    printArray(originalArray)
 
 
 if __name__ == "__main__":
