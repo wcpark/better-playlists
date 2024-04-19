c1a69fed1bf6b9c272875d727a7bb6d30168d726
William Park <72711764+wcpark@users.noreply.github.com>
2024-04-18 21:25:27 -0400

    Change quick sort to take in list of tuples, where first value is playlist position and second is match percent; also, add 100,000 value test

--- src/quicksort.py
+++ src/quicksort.py
@@ -1,62 +1,61 @@
 #!/usr/bin/python3
 
+import random
 import time
 
 
 # Slides from class used as reference
 # Function to swap position of elements
-def swap(originalArray, i, j):
-    originalArray[i], originalArray[j] = originalArray[j], originalArray[i]
+def swap(originalList, i, j):
+    originalList[i], originalList[j] = originalList[j], originalList[i]
 
 
-# Function to partition the array on the basis of pivot element
-def partition(originalArray, low, high):
+# Function to partition the list on the basis of pivot element
+def partition(originalList, low, high):
     # Select the pivot element
-    pivot = originalArray[low]
+    pivot = originalList[low][1]
     up = low
     down = high
 
     while up < down:
-        for j in range(up, high):
-            if originalArray[j] > pivot:
-                break
+        while up <= high and originalList[up][1] <= pivot:
             up += 1
-        for j in range(high, low, -1):
-            if originalArray[j] < pivot:
-                break
+        while down >= low and originalList[down][1] > pivot:
             down -= 1
         if up < down:
-            swap(originalArray, up, down)
-    swap(originalArray, low, down)
+            swap(originalList, up, down)
+    swap(originalList, low, down)
     return down
 
 
-def quickSort(originalArray, low, high):
+def quickSort(originalList, low, high):
     if low < high:
-        pivot = partition(originalArray, low, high)
-        quickSort(originalArray, low, pivot - 1)
-        quickSort(originalArray, pivot + 1, high)
+        pivot = partition(originalList, low, high)
+        quickSort(originalList, low, pivot - 1)
+        quickSort(originalList, pivot + 1, high)
 
 
-# Function to print elements of an array
-def printArray(originalArray):
-    for i in originalArray:
+# Function to print elements of an list
+def printList(originalList):
+    for i in originalList:
         print(i, end=" ")
 
 
 def main():
     # Example use
-    data = [0.39, 6.13, 22.89, 3.14, 0.12, 5.73, 1.00]
+
+    # Generate 100,000 tuples list for testing using list comprehension
+    originalList = [(i, random.uniform(0, 1)) for i in range(1, 100001)]
 
     start_time = time.time()
-    quickSort(data, 0, len(data) - 1)
+    quickSort(originalList, 0, len(originalList) - 1)
     end_time = time.time()
 
     sort_time = end_time - start_time
 
+    print("Sorted list: ", end="")
+    printList(originalList)
     print("Time taken for quick sort:", sort_time, "seconds")
-    print("Sorted array: ", end="")
-    printArray(data)
 
 
 if __name__ == "__main__":
